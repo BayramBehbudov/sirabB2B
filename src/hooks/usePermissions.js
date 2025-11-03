7b56406f1126a9b547482d7455ce43d3de474714
Bayram <bayram.behbudov@gmail.com>
2025-11-03 11:06:48 +0400

    update

--- src/hooks/usePermissions.js
+++ src/hooks/usePermissions.js
@@ -75,32 +75,3 @@ const usePermissions = (obj) => {
 
 export default usePermissions
 
-
-// 1. Auth: UserId - yə görə permission qrupların siyahısı
-// 2. Auth: İstifadəçini permission qrupa əlavə et / sil
-// 3. Auth: İstifadəçilər siyahısı
-// 4. Auth: İstifadəçi məlumatlarına id - yə görə baxmaq
-// 5. Auth: İstifadəçini deaktiv / aktiv etmək
-// 6. Auth: İstifadəçi yaratmaq
-// 7. Auth: İstifadəçi şifrə yeniləmə
-// 8. B2BMüştərilər: Müştərilər listi
-// 9. B2BMüştərilər: Müştəri məlumatı
-// 10. B2BMüştərilər: Müştəri aktiv / deaktiv etmə
-// 11. B2BMüştərilər: B2BMüştəri yaratma
-// 12. B2BMüştərilər: Admin B2BMüştəri məlumatlarını yeniləmə
-// 13. B2BMüştərilər: B2BMüştəri şifrə yeniləmə
-// 14. B2BMüştərilər: Sirab tərəfindən B2BMüştəri məlumatlarını təsdiqləmə
-// 15. B2BCustomerGroup: Müştəri qrupu siyahısı
-// 16. B2BCustomerGroup: Müştəri qrupu məlumatları id - yə görə sorğulama
-// 17. B2BCustomerGroup: Müştəri qrupu yaratmaq
-// 18. B2BCustomerGroup: Müştəri qrupu düzəliş etmək
-// 19. B2BCustomerGroup: Müştəri qrupu silmək
-// 20. Banner: Banner yaratmaq
-// 21. Banner: Bannerlərin siyahısı
-// 22. Banner: Banner məlumatı İd - yə görə baxmaq
-// 23. Banner: Banner məlumatına müştəriyə görə baxmaq
-// 24. Inventory: Inventar siyahısı
-// 25. Inventory: Inventar id - yə görə məlumata baxmaq
-// 26. Inventory: Inventar yaratmaq
-// 27. Inventory: Inventar düzəliş etmək
-// 28. Inventory: Inventar silmək
